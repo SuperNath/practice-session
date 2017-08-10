3f25e9365ebef77f8ec59262b40bb62d5dbb3d30
SuperNath <30775264+SuperNath@users.noreply.github.com>
2017-08-10 11:27:19 +0530

    Add files via upload

---
+++ email_timer/test_mail.php
@@ -0,0 +1,42 @@
+<?php
+
+error_reporting(-1);
+ini_set('display_errors','ON');
+
+		// email send
+
+	include_once("../../../../../usr/share/php/libphp-phpmailer/class.phpmailer.php");
+	//require 'system/includes/PHPMailer/PHPMailerAutoload.php';
+
+	
+	$mail = new PHPMailer();
+
+	$mail->isSMTP();                                 
+	$mail->Host = 'smtp.gmail.com';  		
+	$mail->SMTPAuth = true;                              
+	$mail->Username = 'vyrazulabs@gmail.com';            
+	$mail->Password = 'VasuNaman';                           
+	$mail->SMTPSecure = 'ssl';                
+	$mail->Port = 465;                                    
+
+	$mail->From = 'vyrazulabs@gmail.com';
+	$mail->FromName = 'Mailer';
+	$mail->addAddress('vyrazulabs@gmail.com', 'Vasu Naman');  
+
+	//Send HTML or Plain Text email
+	$mail->isHTML(true);
+
+	$mail->Subject = 'Here is the subject';
+	$mail->Body    = 'This is the HTML message body <b> in Bold </b>';
+	$mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+
+	if(!$mail->send()) {
+	   echo 'Message could not be sent.';
+	   echo 'Mailer Error: ' . $mail->ErrorInfo;
+	   exit;
+	}
+
+	//$mail->send();
+	echo '<p>Message has been sent</p>';
+	
+?>
---
+++ email_timer/timer.php
@@ -0,0 +1,79 @@
+<?php
+	include_once("../../../../../usr/share/php/libphp-phpmailer/class.phpmailer.php");
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Email Send</title>
+	<script type="text/javascript">
+		var i=0;
+		
+		function timer()
+		{
+			++i;
+
+			if(i<61)
+			{
+				document.getElementById("timer").innerHTML = i;
+			}
+			else if(i == 61)
+			{
+				window.location.href = "timer.php?action=send";
+			}
+		}
+
+		setInterval(function(){ timer(); },1000);
+
+		function timerOut()
+		{
+			document.getElementById("send").style.display="none";
+		}
+
+		setTimeout(function(){ timerOut(); },5000);
+	</script>
+</head>
+<body>
+<center>
+	<h1 style="font-size:100px;">Timer</h1>
+	<span id="timer" style="font-size:120px;"></span>
+
+	<?php
+		// email send
+
+	if(isset($_GET["action"]))
+	{	
+		$mail = new PHPMailer();
+
+		$mail->isSMTP();                                 
+		$mail->Host = 'smtp.gmail.com';  		
+		$mail->SMTPAuth = true;                              
+		$mail->Username = 'vyrazulabs@gmail.com';            
+		$mail->Password = 'VasuNaman';                           
+		$mail->SMTPSecure = 'ssl';                
+		$mail->Port = 465;                                    
+
+		$mail->From = 'vyrazulabs@gmail.com';
+		$mail->FromName = 'Mailer';
+		$mail->addAddress('vyrazulabs@gmail.com', 'Vasu Naman');  
+
+		//Send HTML or Plain Text email
+		$mail->isHTML(true);
+
+		$mail->Subject = 'Here is the subject';
+		$mail->Body    = 'This is the HTML message body <b> in Bold </b>';
+		$mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+
+		if(!$mail->send()) {
+		   echo 'Message could not be sent.';
+		   echo 'Mailer Error: ' . $mail->ErrorInfo;
+		   exit;
+		}
+
+		//$mail->send();
+		echo '<p id="send" style="color:green;font-weight:bold;font-size:120px;">Message has been sent</p>';
+	}
+
+	?>
+</center>
+</body>
+</html>
